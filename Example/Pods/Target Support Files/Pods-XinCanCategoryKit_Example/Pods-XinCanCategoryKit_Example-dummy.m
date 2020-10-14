dd010bb2074f6c063f6838997357970b7f39f86d
刘兆亮 <liuzhaoliang@daling.com>
2020-10-14 17:49:41 +0800

    123

---
+++ Example/Pods/Target Support Files/Pods-XinCanCategoryKit_Example/Pods-XinCanCategoryKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XinCanCategoryKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XinCanCategoryKit_Example
+@end
---
+++ Example/Pods/Target Support Files/XinCanCategoryKit/XinCanCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XinCanCategoryKit : NSObject
+@end
+@implementation PodsDummy_XinCanCategoryKit
+@end
